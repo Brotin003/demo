28201cf187a4e771d4aee14d38482322a251b13f
@amangwari <65161800+Amangwari@users.noreply.github.com>
2020-10-01 15:08:14 +0530

    Update numberprogs.py

--- numberprogs.py
+++ numberprogs.py
@@ -1,8 +1,8 @@
-n=int(input("Enter the no of students:"))  
+n=int(input("Enter the no of students in class:"))  
 dict1={}
 for i in range(n):
     key=input("Name of the student")
-    value=int(input("Enter the percentage"))
+    value=int(input("Enter the %age"))
     dict1[key]=value
 lst=dict1.values()  
 
